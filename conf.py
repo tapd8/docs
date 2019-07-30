db44248574445b287ed860c27ba029a57b320f21
MiracleYoung <miracleyoung0723@gmail.com>
2019-07-30 17:00:33 +0800

    update sphinx_doc

--- conf.py
+++ conf.py
@@ -65,9 +65,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_css_files = [
-    '_static/custom.css',
-]
 
 
 source_suffix = {
